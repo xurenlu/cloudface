664e4a4f0a7ac7e983667fb87e6683bd9240875d
xurenlu <xurenlu@gmail.com>
2010-03-01 21:16:09 +0800

    nothing

--- psearch/rpc_search.py
+++ psearch/rpc_search.py
@@ -6,12 +6,8 @@ __author__ = "162cm <xurenlu@gmail.com>"
 
 import sys,os
 sys.path.append("/usr/local/lib/python2.6/dist-packages/phprpc-3.0.0-py2.6.egg/phprpc")
-#from phprpc import PHPRPC_Server # 引入 PHPRPC Server
 from phprpc import PHPRPC_WSGIApplication
-#, UrlMapMiddleware, PHPRPC_Server
 import datetime
-#from pymmseg import mmseg
-#import xappy
 from flup.server.fcgi import WSGIServer
 import optparse
 import yaml
@@ -23,20 +19,6 @@ FCGI_SOCKET_DIR = '/tmp'
 FCGI_SOCKET_UMASK = 0111
 DB_PREFIX="./"
 
-#mmseg.dict_load_defaults()
-
-'''
-#create_index( "./datadir/", ["text"], ["author","desc","text","categoryid"], ["categoryid"])
-#index_data("./datadir/", "book3", { "author":"xurenlu", "desc":"这是一本好书", "text":"故事发生在20年以前", "categoryid":123 },["desc","text"])
-#index_data("./datadir/", "book2", { "author":"xurenlu", "desc":"好书不多见", "text":"故事很不错哟12块钱", "categoryid":12},["desc","text"])
-report=search_result_report("./datadir/","故事")
-print(report)
-sout=(search("./datadir/","故事",0,10))
-print(sout)
-for item in sout:
-    for key in item.data:
-        print key,":",item.data[key][0]
-'''
 def _get_socket_path(name, server_number):
     """return socket file path for fastcgi"""
     return os.path.join(FCGI_SOCKET_DIR, 'fcgi-%s-%s.socket' % (name, server_number))
@@ -276,49 +258,6 @@ def main(args_in, app_name="api"):
     if not opt.server_num:
         opt.server_num=1
     
-    #print _get_methods(sys.modules[__name__])
-    #sys.exit(0)
-#    db_init("test",[ {"prefix":"A", "field":"author", "segment":False}, {"prefix":"S", "field":"subject", "segment":False}, {"prefix":"I", "field":"id", "segment":False } ])
-    #_prepare_scws()
-    #import sys
-    #db_initsimple('simple1')
-    #print _load_dbdesc("simple1")
-    #value={"author":"renlu.xu","date":"2009-02-32","email":"xurenlu@gmail.com","url":"http://www.sohu.com/","title":"中国和美国谈判了","content":"前天国家主席胡家明访问了美国东部地区"}
-    #key="doc_1"
-    #db_init("simple2",{"author":{"prefix":"A","segment":False},"date":{"prefix":"D","segment":False},"email":{"prefix":"E","segment":False},"url":{"prefix":"U","segment":False},"title":{"prefix":"T","segment":True},"content":{"prefix":"C","segment":True}})
-    #print db_index("simple2",key,value)
-
-    #db_initsimple("simple1")
-    #db_simpleindex("simple1","z0",'我们是好朋友 I love china')
-    #db_simpleindex("simple1","z1",'到我们来自美国 ,你来自哪里?')
-    #db_simpleindex("simple1","z2",'哈我们喜欢中美你来自哪里?')
-    #db_simpleindex("simple1","z3",'w你来我们这里吧')
-    #db_simpleindex("simple1","z4",'哈哈,我们这里是最好的公园')
-    #db_simpleindex("simple1","z5",'屁我们都要去法国')
-    #db_simpleindex("simple1","z6",'考虑我们一起走')
-    #db_simpleindex("simple1","z7",'我们才是好的')
-    #db_simpleindex("simple1","z8",'就是我们怎么了啊?')
-    #db_simpleindex("simple1","z9",'我们')
-    #db_simpleindex("simple1","z10",'我们最喜欢这个')
-    #db_simpleindex("simple1","z11",'一样我们也是好样的')
-    #db_simpleindex("simple1","z12",'一起我们哈哈')
-    #db_simplesearch_count("simple1","我们")
-    #db_simplesearch_count("simple1","他们")
-    #print db_simplesearch_count("simple1","I")
-    #print db_simplesearch_count("simple1","他们")
-    #print db_generic_search("simple1",{"text":"我们佛教徒不吃肉"})
-    #sys.exit(0)
-    #db_simplesearch("simple1","'text:我们'",0,10)
-    #sys.exit(0)
-    #db=_get_rdb("simple1")
-    #for c in db.allterms():
-    #    print "new term",c.term
-    #print dir(db)
-    #print db_get_doccount("test")
-    #print db_simplesearch_count("simple1","我们")
-    #print db_simplesearch("simple1","我们",2,5)
-    #print db.get_document(2).get_data()
-    #_free_scws()
 
     socketfile = _get_socket_path(app_name, opt.server_num)
     app=PHPRPC_WSGIApplication()
